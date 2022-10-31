9384d80e2738303e03afd8347a6742e7ef2eebb0
Undeadtaker <filipkasic@gmail.com>
2022-10-30 23:05:35 -0700

    Testing the premake library with the project, created side branch for it which will hopefully be merged with main once all the conflicts have been taken care of.

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Hazel"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-x64"
+
+project "Hazel-CPY"
+	
+	location "Hazel-CPY"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediaries/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include;"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"	
+		staticruntime "On"
+		systemversion "10.0"
+
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} Hazel-CPY/bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediaries/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.hpp",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel-CPY/vendor/spdlog/include",
+		"Hazel-CPY/src"
+	}
+
+	links
+	{
+		"Hazel-CPY"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"	
+		staticruntime "On"
+		systemversion "10.0"
+
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
